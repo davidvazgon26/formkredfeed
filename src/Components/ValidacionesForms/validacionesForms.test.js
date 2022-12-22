5828aef778c9054ad1ec78510aeaed0034ef7cb8
davidvazgon26 <davidvazgon@hotmail.com>
2022-12-22 01:52:20 -0600

    se crean archivos de test y readme

---
+++ src/Components/ValidacionesForms/validacionesForms.test.js
@@ -0,0 +1,12 @@
+test('Jest funciona correctamente', () => {
+    // Arrange
+    const num1 = 2;
+    const num2 = 3;
+  
+    // Act
+    const result = num1 + num2;
+  
+    // Assert
+    expect(result).toBe(5);
+  });
+  
